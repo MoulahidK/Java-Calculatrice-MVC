554a44c92fad8580fbed1dc40160f296aeeae487
JosueAbou <69512506+JosueAbou@users.noreply.github.com>
2021-10-20 15:24:46 +0200

    modification de ceratines méthodes
    Ajout des conditions de vérifications

--- src/Accumulateur.java
+++ src/Accumulateur.java
@@ -18,36 +18,56 @@ public class Accumulateur implements IAccumaleur {
 
     @Override
     public void swap() {
-        Double variableDepile1 = maPile.pop();
-        Double variableDepile2 = maPile.pop();
-        push(variableDepile1);
-        push(variableDepile2);
+        if(maPile.size()>=2) {
+            Double variableDepile1 = maPile.pop();
+            Double variableDepile2 = maPile.pop();
+            push(variableDepile1);
+            push(variableDepile2);
+        }
     }
 
     @Override
     public void add() {
-        Double variableDepile1 = maPile.pop();
-        Double variableDepile2 = maPile.pop();
-        Double variableAddition = variableDepile1 + variableDepile2;
-        push(variableAddition);
+        if(maPile.size()>=2) {
+            Double variableDepile1 = maPile.pop();
+            Double variableDepile2 = maPile.pop();
+            Double variableAddition = variableDepile1 + variableDepile2;
+            push(variableAddition);
+        }
     }
 
     @Override
     public void sub() {
-        Double variableDepile1 = maPile.pop();
-        Double variableDepile2 = maPile.pop();
-        Double variableSoustraction = variableDepile1 - variableDepile2;
-        push(variableSoustraction);
+        if(maPile.size()>=2) {
+            Double variableDepile1 = maPile.pop();
+            Double variableDepile2 = maPile.pop();
+            Double variableSoustraction = variableDepile1 - variableDepile2;
+            push(variableSoustraction);
+        }
     }
 
     @Override
     public void mult() {
-
+        if(maPile.size()>=2) {
+            Double variableDepile1 = maPile.pop();
+            Double variableDepile2 = maPile.pop();
+            Double variableMultiplication = variableDepile1 * variableDepile2;
+            push(variableMultiplication);
+        }
     }
 
     @Override
     public void div() {
-
+        if(maPile.size()>=2) {
+            Double variableDepile1 = maPile.pop();
+            Double variableDepile2 = maPile.pop();
+            if (variableDepile2 == 0) {
+                System.out.print("ZeroDivision Error");
+            } else {
+                Double variableDivision = variableDepile1 / variableDepile2;
+                push(variableDivision);
+            }
+        }
     }
 
     @Override
