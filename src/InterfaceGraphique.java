6fa09c5e91d5bcb8c065441e10b132c8f70836b3
JosueAbou <69512506+JosueAbou@users.noreply.github.com>
2021-10-22 17:42:42 +0200

    Modification certaines classes
    Finition du modèle et du controlleur.
    Début de la View

---
+++ src/InterfaceGraphique.java
@@ -0,0 +1,9 @@
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+
+public class InterfaceGraphique extends Scene {
+
+    public InterfaceGraphique(){
+        super(new StackPane());
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
 public class Main {
+
     public Main(){
+    }
+
+    public static void main(String[] args){
         Accumulateur mAccumulateur = new Accumulateur();
 
+        mAccumulateur.accumuler('1');
+        mAccumulateur.push();
+        mAccumulateur.accumuler('2');
+        mAccumulateur.push();
 
+        System.out.print(mAccumulateur.maPile.pop());
+        System.out.print("\n");
+        System.out.print(mAccumulateur.maPile.pop());
+
+        //mAccumulateur.add();
+
+        //System.out.print(mAccumulateur.maPile.pop());
 
     }
 }
